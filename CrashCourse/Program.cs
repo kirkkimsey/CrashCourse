a765957d334fb31d9733fe11de8deb3d7cdb22ba
kkimse1326 <kkimse1326@WS-AC-CTD-37008>
2017-01-05 09:29:54 -0500

    Declaring data types and values.

--- CrashCourse/Program.cs
+++ CrashCourse/Program.cs
@@ -11,7 +11,18 @@ namespace CrashCourse
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            int number = 2;
+            Double number1 = 3.4;
+            Char letter = 'a';
+            String message = "Hello World";
+            bool decision = true;
+            Console.WriteLine("Number: " + number);
+            Console.WriteLine("Number1: " + number1);
+            Console.WriteLine("Letter: " + letter);
+            Console.WriteLine("Message: " + message);
+            Console.WriteLine("Decision: " + decision);
             Console.Read();
+
         }
     }
 }
