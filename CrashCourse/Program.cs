90b58c5173dc932b286e1fa7828d2d2ac660eea6
kkimse1326 <kkimse1326@WS-AC-CTD-37008>
2017-01-10 08:44:08 -0500

    Switch Case, winningNumber

--- CrashCourse/Program.cs
+++ CrashCourse/Program.cs
@@ -10,33 +10,54 @@ namespace CrashCourse
     {
         static void Main(string[] args)
         {
-       
-
             //Give Instruction
-            Console.WriteLine("Pick a number 1-3.");
-         
-
-            //Declare winning number
-            int winningNumber = 2;
-
-            //Accept string from user
-            string data = Console.ReadLine();
-
-            //Convert string to int
-            int answer;
-            int.TryParse(data, out answer);
-
-            //Compare user input with declared value
-            if (answer == winningNumber)
-            {
-                Console.WriteLine("You Win.");
-            }
-            else
-            {
-                Console.WriteLine("Sorry, you are wrong.");
+            Console.WriteLine("Pick a number (1-5)");
+            //Declare winning numbers
+            const int winningNumber1 = 3;
+            const int winningNumber2 = 5;
+            //collect user date
+            string userData = Console.ReadLine();
+            int userNumber;
+            int.TryParse(userData, out userNumber);
+            //do they match?
+            switch (userNumber) {
+                case winningNumber1:
+                Console.WriteLine("You win!");
+                    break;
+                case winningNumber2:
+                   Console.WriteLine("you kind of win");
+                    break;
+                default:
+                    Console.WriteLine("You Lose");
+                    break;
             }
             Console.Read();
-            }
+
+            ////Give Instruction
+            //Console.WriteLine("Pick a number 1-3.");
+
+
+            ////Declare winning number
+            //int winningNumber = 2;
+
+            ////Accept string from user
+            //string data = Console.ReadLine();
+
+            ////Convert string to int
+            //int answer;
+            //int.TryParse(data, out answer);
+
+            ////Compare user input with declared value
+            //if (answer == winningNumber)
+            //{
+            //    Console.WriteLine("You Win.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sorry, you are wrong.");
+            //}
+            //Console.Read();
         }
+    }
     }
 
