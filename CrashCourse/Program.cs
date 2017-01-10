c4d2b8bb4f6541ff15932defc5edaa103c1a887a
kkimse1326 <kkimse1326@WS-AC-CTD-37008>
2017-01-10 10:06:15 -0500

    FizzBuzz

--- CrashCourse/Program.cs
+++ CrashCourse/Program.cs
@@ -11,41 +11,28 @@ namespace CrashCourse
     {
         static void Main(string[] args)
         {
-            //bool playGame = true;
-            //do
-            //{
-
-            //}
-            //while (playGame);
-
-            //int lives = 5;
-            //for (int i = 0; i < lives; i++)
-            //{
-
-            //}
-
-            //string[] stuff = new string[5];
-
-            ArrayList stuff = new ArrayList();
-            stuff.Add("cat");
-            stuff.Add("dog");
-            stuff.Add("squirrel");
-
-            List<string> stuff2 = new List<string>();
-            stuff2.Add("cat");
-            stuff2.Add("dog");
-            stuff2.Add("squirrel");
-            //stuff2.RemoveAt(1);
-            //stuff2.RemoveAt(stuff2.IndexOf("squirrel"));
-            var index = stuff2.IndexOf("cat");
-            if (index > 0)
+    
+            for (int i = 0; i <= 100; i++)
             {
-                stuff2.RemoveAt(index);
-            }
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
 
-            foreach (var animal in stuff2)
-            {
-                Console.WriteLine(animal);
+                if (fizz && buzz)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                }
+                else if (fizz)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (buzz)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else 
+                        {
+                    Console.WriteLine(i);
+                }  
             }
             Console.Read();
         }
